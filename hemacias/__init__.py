2c86476c6a7baac220715b30b25869af801b57b2
Marcelo Maurin Martins <marcelomaurinmartins@gmail.com>
2026-09-19 11:04:10 -0300

    visao: exporta classes watershed

--- hemacias/__init__.py
+++ hemacias/__init__.py
@@ -1,5 +1,14 @@
 """Contador de hemácias por visão computacional."""
 
 from .counter import CellCounter, CounterConfig, DetectionResult
+from .watershed import WatershedCounter, WatershedConfig, WatershedResult, SegmentedCell
 
-__all__ = ["CellCounter", "CounterConfig", "DetectionResult"]
+__all__ = [
+    "CellCounter",
+    "CounterConfig",
+    "DetectionResult",
+    "WatershedCounter",
+    "WatershedConfig",
+    "WatershedResult",
+    "SegmentedCell",
+]
